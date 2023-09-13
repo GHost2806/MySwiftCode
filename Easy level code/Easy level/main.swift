81f88e63d30b19df0ddfd165efdd62ad378548e2
Macbook Air <kornilov.s2001@yandex.ru>
2023-09-13 17:41:26 +0500

    Update

--- Easy level code/Easy level/main.swift
+++ Easy level code/Easy level/main.swift
@@ -393,4 +393,15 @@ if maxWeight < totalWeight {
 }
 
 //26 Lazy Ленивые процессы
+func bigDataProcessingFunc() -> String {
+    return "very long process"
+}
 
+class Processing {
+    let smallDataProcessing = "small data processing"
+    let averageDataProcessing = "average data processing"
+    lazy var bigDataProcessing = bigDataProcessingFunc()
+}
+
+let process = Processing()
+print (process.bigDataProcessing)
